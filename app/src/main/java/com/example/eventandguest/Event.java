51d3172e8f8da19e310c0570207ff7fbb1c1f328
Lydia Astrella <lydiaastrella@gmail.com>
2020-04-11 21:10:51 +0700

    add Event class

---
+++ app/src/main/java/com/example/eventandguest/Event.java
@@ -0,0 +1,76 @@
+package com.example.eventandguest;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Event implements Parcelable {
+    private int id;
+    private String name, date, image;
+
+    public Event() {
+    }
+
+    protected Event(Parcel in) {
+        id = in.readInt();
+        name = in.readString();
+        date = in.readString();
+        image = in.readString();
+    }
+
+    public static final Creator<Event> CREATOR = new Creator<Event>() {
+        @Override
+        public Event createFromParcel(Parcel in) {
+            return new Event(in);
+        }
+
+        @Override
+        public Event[] newArray(int size) {
+            return new Event[size];
+        }
+    };
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(id);
+        parcel.writeString(name);
+        parcel.writeString(date);
+        parcel.writeString(image);
+    }
+}
