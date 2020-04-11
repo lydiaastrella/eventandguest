114a019b628eb7621d8068a10340057e6f1cb38b
Lydia Astrella <lydiaastrella@gmail.com>
2020-04-11 23:29:17 +0700

    add recycler view adapter

--- app/src/main/java/com/example/eventandguest/EventActivity.java
+++ app/src/main/java/com/example/eventandguest/EventActivity.java
@@ -1,14 +1,39 @@
 package com.example.eventandguest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class EventActivity extends AppCompatActivity {
+
+    RecyclerView rv;
+    private ArrayList<Event> list;
+    final String STATE_LIST = "state_list";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
+
+        rv = findViewById(R.id.rv_category);
+        rv.setHasFixedSize(true);
+
+        list = new ArrayList<>();
+
+        if(savedInstanceState == null){
+            list.addAll(EventData.getListData());
+        }else{
+            ArrayList<Event> stateList = savedInstanceState.getParcelableArrayList(STATE_LIST);
+            list.addAll(stateList);
+        }
+
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        ListEventAdapter listEventAdapter = new ListEventAdapter(this);
+        listEventAdapter.setListPresident(list);
+        rv.setAdapter(listEventAdapter);
     }
 }
